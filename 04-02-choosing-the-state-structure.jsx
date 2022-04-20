292bc29f2f140a90136ab778a6cd94b90b9bf747
Shinyoung Park <shinyouwith@gmail.com>
2022-04-20 23:30:03 +0900

    read(react-docs-beta): Choosing the State Structure 3/3

--- 04-02-choosing-the-state-structure.jsx
+++ 04-02-choosing-the-state-structure.jsx
@@ -37,3 +37,160 @@ const [position, setPosition] = useState({
   x: 0,
   y: 0
 });
+
+/*
+2 Avoid contradictions in state
+
+- `isSending`과 `isSent`같은 상태 2가지를 함께 사용하면, 
+  - setIsSent, setIsSending을 같이 호출하지 않을 경우에 `isSending`과 `isSent`이 둘 다 true인 있을 수 없는 불가능한 상황이 발생하게 된다. 
+  - 이런 상황이 애초에 생기지 않도록, `status`라는 하나의 상태 변수를 두고 3가지 가능한 상태만을 가질 수 있게 수정할 수 있다. 'typing', 'sending', 'sent'같이. 
+*/
+
+import { useState } from 'react';
+
+export default function FeedbackForm() {
+  const [text, setText] = useState('');
+  const [status, setStatus] = useState('typing');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('sending');
+    await sendMessage(text);
+    setStatus('sent');
+  }
+
+  const isSending = status === 'sending';
+  const isSent = status === 'sent';
+
+  if (isSent) {
+    return <h1>Thanks for feedback!</h1>
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>How was your stay at The Prancing Pony?</p>
+      <textarea
+        disabled={isSending}
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <br />
+      <button
+        disabled={isSending}
+        type="submit"
+      >
+        Send
+      </button>
+      {isSending && <p>Sending...</p>}
+    </form>
+  );
+}
+
+function sendMessage(text) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000);
+  });
+}
+
+/*
+3 Avoid redundant state
+
+- 컴포넌트의 props나 이미 있는 상태 변수들로부터 계산할 수 있는 정보라면, 별도의 컴포넌트 상태로 관리하지 말 것
+- props로 내려받은 값을 다시 컴포넌트의 state에 재할당하지 말 것! (mirroring props)
+  - props가 업데이트되어도, 컴포넌트의 지역 상태는 업데이트되지 않는다. 
+    - 의도적으로 모든 업데이트를 무시하기 위한 용도라면 사용한다. 초기값만 props로 받는다거나.
+        function Message({ initialColor }) {
+          const [color, setColor] = useState(initialColor);
+  - 만약 이름을 더 알맞게 바꾸고 싶은 거라면, 그냥 const를 사용해서 이름만 바꾸기.
+    function Message({ messageColor }) {
+      const color = messageColor;
+*/
+
+import { useState } from 'react';
+
+export default function Form() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+    setFullName(e.target.value + ' ' + lastName);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+    setFullName(firstName + ' ' + e.target.value);
+  }
+
+  return (
+    <>
+      <h2>Let’s check you in</h2>
+      <label>
+        First name:{' '}
+        <input
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:{' '}
+        <input
+          value={lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+      <p>
+        Your ticket will be issued to: <b>{fullName}</b>
+      </p>
+    </>
+  );
+}
+
+import { useState } from 'react';
+
+export default function Form() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const fullName = firstName + ' ' + lastName;
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>Let’s check you in</h2>
+      <label>
+        First name:{' '}
+        <input
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:{' '}
+        <input
+          value={lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+      <p>
+        Your ticket will be issued to: <b>{fullName}</b>
+      </p>
+    </>
+  );
+}
+
+/*
+4 Avoid duplication in state
+*/
+
+/*
+5 Avoid deeply nested state
+*/
